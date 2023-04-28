ef8701d6453fa0570fd8aef55d391de207177e2f
bhagtanib <bbhagtani23@my.whitworth.edu>
2023-04-28 11:06:29 -0700

    Added Table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import SideBarr from './components/SideBarr';
+import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <div className='sidetest'>
+        <SideBarr />
+        <Table />
+      </div>
     </div>
   );
 }
